e7714f9298dd218b0f8ba572f2f1e8de268e7f6c
zhongfly <11155705+zhongfly@users.noreply.github.com>
2023-12-23 00:06:15 +0800

    configure ccache before restore cache

--- src/main.ts
+++ src/main.ts
@@ -228,6 +228,10 @@ export default async function main(): Promise<void> {
       await checkCcacheAvailability();
     });
 
+    await Core.group("Configure ccache", async () => {
+      await configureCcache();
+    });
+
     let cacheHit = false;
     if (Core.getBooleanInput("restore_cache"))
       cacheHit = await restoreCache();
@@ -236,10 +240,6 @@ export default async function main(): Promise<void> {
 
     await Core.group(`Set output variable: cache_hit="${cacheHit}"`, async () => {
       Core.setOutput("cache_hit", cacheHit.toString());
-    });
-
-    await Core.group("Configure ccache", async () => {
-      await configureCcache();
     });
 
     await Core.group("Clear ccache statistics", async () => {
